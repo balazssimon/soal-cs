69d0eca83729ca3a29797b21de3360f0b5fef1fd
Balazs Simon <balazssimon@gmail.com>
2024-11-19 01:06:14 +0100

    Diagnostic descriptors fixed

--- Src/Main/MetaDslx.Languages.Soal/Importer/SoalImporterErrorCode.cs
+++ Src/Main/MetaDslx.Languages.Soal/Importer/SoalImporterErrorCode.cs
@@ -14,7 +14,7 @@ namespace MetaDslx.Languages.Soal.Importer
     public class SoalImporterErrorCode
     {
         public static readonly DiagnosticDescriptor ERR_SoalImport = DiagnosticDescriptor.Error(nameof(ERR_SoalImport), "Error", "{0}");
-        public static readonly DiagnosticDescriptor WRN_SoalImport = DiagnosticDescriptor.Error(nameof(WRN_SoalImport), "Warning", "{0}");
-        public static readonly DiagnosticDescriptor INF_SoalImport = DiagnosticDescriptor.Error(nameof(INF_SoalImport), "Info", "{0}");
+        public static readonly DiagnosticDescriptor WRN_SoalImport = DiagnosticDescriptor.Warning(nameof(WRN_SoalImport), "Warning", "{0}");
+        public static readonly DiagnosticDescriptor INF_SoalImport = DiagnosticDescriptor.Info(nameof(INF_SoalImport), "Info", "{0}");
     }
 }
